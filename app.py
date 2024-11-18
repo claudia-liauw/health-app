345ebe26393737d8a3b56c31cba48361ffc36e35
Claudia <87799252+claudia-liauw@users.noreply.github.com>
2024-11-18 22:30:15 +0800

    implement log in and log out

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+db_path = "data/users.db"
+
 @app.route("/")
 def index():
     hourly_steps = pd.read_csv('data/fitbit_apr/hourlySteps_merged.csv')
@@ -58,7 +60,7 @@ def heart_rate():
 def register():
     """Register user"""
     if request.method == "POST":
-        with sqlite3.connect("data/users.db") as db:
+        with sqlite3.connect(db_path) as db:
             username = request.form['username']
             password = request.form['password']
             confirmation = request.form['confirmation']
@@ -77,7 +79,7 @@ def register():
 
             hash = generate_password_hash(password)
             try:
-                db.executemany("INSERT INTO users (username, hash) VALUES (?, ?)", [(username, hash)])
+                db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", (username, hash))
                 db.commit()
                 return redirect("/")
             # If username already exists
@@ -87,3 +89,52 @@ def register():
 
     else:
         return render_template("register.html")
+    
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return render_template("login.html", invalid="Must provide username!")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return render_template("login.html", invalid="Must provide password!")
+
+        # Query database for username
+        with sqlite3.connect("data/users.db") as db:
+            rows = db.execute(
+                "SELECT * FROM users WHERE username = ?", (request.form.get("username"),)
+            ).fetchall()
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0][1], request.form.get("password")
+        ):
+            return render_template("login.html", invalid='Invalid username or password!')
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0][0]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+    
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
