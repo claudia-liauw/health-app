ac868068c7c351c3391400477bd0116b40b0fa29
Claudia <87799252+claudia-liauw@users.noreply.github.com>
2025-06-28 23:26:34 +0800

    change database path to tmp for write permissions

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-DB_PATH = "data/users.db"
+DB_PATH = "/tmp/users.db"
 TODAY_DATE = datetime.date.today()
 
 # create database if it doesn't exist
